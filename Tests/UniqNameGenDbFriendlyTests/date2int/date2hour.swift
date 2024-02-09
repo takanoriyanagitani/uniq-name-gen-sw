3d27175c8a6c7650b7c640817718e18d096e0392
yanagitani <takanori.yanagitani@gmail.com>
2024-02-10 08:25:23 +0900

    test: time zone independent test

--- Tests/UniqNameGenDbFriendlyTests/date2int/date2hour.swift
+++ Tests/UniqNameGenDbFriendlyTests/date2int/date2hour.swift
@@ -8,16 +8,26 @@ final class TestHour: XCTestCase {
 
     func test_epoch(){
         let d: Date = UniqNameGenDbFriendly.unixtime2date(unixtime: 0.0)
-        let c: Calendar = Calendar.current
+        var c: Calendar = Calendar.current
+        c.timeZone = TimeZone(secondsFromGMT: 0)!
         let i: Int = UniqNameGenDbFriendly.date2hour(d: d, c: c)
         XCTAssertEqual(0, i)
     }
 
     func test_simple(){
         let d: Date = UniqNameGenDbFriendly.unixtime2date(unixtime: 39600.0)
-        let c: Calendar = Calendar.current
+        var c: Calendar = Calendar.current
+        c.timeZone = TimeZone(secondsFromGMT: 0)!
         let i: Int = UniqNameGenDbFriendly.date2hour(d: d, c: c)
         XCTAssertEqual(11, i)
+    }
+
+    func test_2006(){
+        let d: Date = ISO8601DateFormatter().date(from: "2006-01-02T15:04:05Z")!
+        var c: Calendar = Calendar.current
+        c.timeZone = TimeZone(secondsFromGMT: 0)!
+        let i: Int = UniqNameGenDbFriendly.date2hour(d: d, c: c)
+        XCTAssertEqual(15, i)
     }
 
 }
