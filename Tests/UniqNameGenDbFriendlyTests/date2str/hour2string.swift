86cf9569bda675862cec658d8db65b6e39e1be10
yanagitani <takanori.yanagitani@gmail.com>
2024-02-13 08:13:10 +0900

    test: more tests

--- Tests/UniqNameGenDbFriendlyTests/date2str/hour2string.swift
+++ Tests/UniqNameGenDbFriendlyTests/date2str/hour2string.swift
@@ -15,4 +15,13 @@ final class TestHour2Str: XCTestCase {
         XCTAssertEqual("00", hs)
     }
 
+    func test_simple(){
+        let d: Date = UniqNameGenDbFriendly.unixtime2date(unixtime: 39600.0)
+        var c: Calendar = Calendar.current
+        c.timeZone = TimeZone(secondsFromGMT: 0)!
+        let hour: Int = UniqNameGenDbFriendly.date2hour(d: d, c: c)
+        let hs: String = UniqNameGenDbFriendly.hour2string(hour: hour)
+        XCTAssertEqual("11", hs)
+    }
+
 }
